21ffb0d4bdafe514bff7569b861e09cc806c23e9
Muhammad Randi Pratama <mrandi96@gmail.com>
2021-01-02 01:59:58 +0700

    add config

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  PORT: 3000,
+  DB: 'postgres://mrtbzvxlifmxbm:ee9c34b394263b8796f580f11237954223874e8ebc505f0b1245e59c198536f5@ec2-52-2-127-5.compute-1.amazonaws.com:5432/db5l22nc94sp2',
+  DEV_DB: '',
+  SALT_ROUND: 10,
+  JWT_SECRET_KEY: 'my#1jwTSup3rS3cr3tK3y()',
+  JWT_EXPIRE_TIME: '3h'
+};
