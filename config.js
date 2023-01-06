b1b21fcd9db475f9850d5c275a42d81a250d8067
Muhammad Randi Pratama <muhammad.pratama@btpn.com>
2023-01-06 20:07:15 +0700

    migrate postgre to supabase

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 module.exports = {
   PORT: 3000,
-  DB: 'postgres://mrtbzvxlifmxbm:ee9c34b394263b8796f580f11237954223874e8ebc505f0b1245e59c198536f5@ec2-52-2-127-5.compute-1.amazonaws.com:5432/db5l22nc94sp2',
+  DB: 'postgresql://postgres:OiJ2mBCU2zqv2DBm@db.swfzpgzdfmdxgffxwuaw.supabase.co:5432/postgres',
+  DB_OLD: 'postgres://mrtbzvxlifmxbm:ee9c34b394263b8796f580f11237954223874e8ebc505f0b1245e59c198536f5@ec2-52-2-127-5.compute-1.amazonaws.com:5432/db5l22nc94sp2',
   DB_LOCAL: 'postgres://postgres:P@ssw0rd123@localhost:5432/tritama',
   SALT_ROUND: 10,
-  JWT_SECRET_KEY: 'my#1jwTSup3rS3cr3tK3y()',
-  JWT_EXPIRE_TIME: '3h',
+  JWT_SECRET_KEY: 'my#1jwT~Sup3r~$3cr3t~K3y()',
+  JWT_EXPIRE_TIME: '12h',
   DEBUG_MODE: false,
   PRODUCTION: false,
   LANG: 'en'
